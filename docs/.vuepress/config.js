4fa35a62f9e5d85f6b44aea62f2ab2e50a6bddc1
zhangxiangliang <admin@pushme.top>
2020-07-17 01:43:59 +0800

    feat: 增加 B股

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,6 +28,7 @@ module.exports = {
         ['/market/一级市场', '一级市场'],
         ['/market/二级市场', '二级市场'],
         ['/market/A股', 'A股'],
+        ['/market/B股', 'B股'],
         ['/market/北上资金', '北上资金'],
         ['/market/南下资金', '南下资金'],
         ['/market/场外基金', '场外基金'],
