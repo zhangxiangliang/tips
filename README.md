e1c7047ea358bcef60be1ef9b7d18c2961aae4f9
zhangxiangliang <admin@pushme.top>
2020-07-17 01:50:17 +0800

    feat: 增加 美股

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 * [A股](docs/market/A股.md)
 * [B股](docs/market/B股.md)
 * [港股](docs/market/港股.md)
+* [美股](docs/market/美股.md)
 * [北上资金](docs/market/北上资金.md)
 * [南下资金](docs/market/南下资金.md)
 * [场外基金](docs/market/场外基金.md)
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,6 +30,7 @@ module.exports = {
         ['/market/A股', 'A股'],
         ['/market/B股', 'B股'],
         ['/market/港股', '港股'],
+        ['/market/美股', '美股'],
         ['/market/北上资金', '北上资金'],
         ['/market/南下资金', '南下资金'],
         ['/market/场外基金', '场外基金'],
